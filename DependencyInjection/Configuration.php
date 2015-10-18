bbfee9e1837f9e7599cde84cad055eb62a3f0ffc
mickadoo <michaeldevery@gmail.com>
2015-10-18 02:43:29 +0200

    further fixes for bundle configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mickadoo_yarnyard_base_data');
+        $rootNode = $treeBuilder->root('mickadoo_base_data');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
--- DependencyInjection/MickadooBaseDataExtension.php
+++ DependencyInjection/MickadooBaseDataExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class MickadooYarnyardBaseDataExtension extends Extension
+class MickadooBaseDataExtension extends Extension
 {
     /**
      * {@inheritdoc}
